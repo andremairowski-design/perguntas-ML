6969833c472e568daaa7dd7ec0f968589b957b69
andremairowski-design <andremairowski@gmail.com>
2026-04-07 11:52:59 -0300

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require("http");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("API rodando 🚀");
+  res.end("OK");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor rodando na porta " + PORT);
+  console.log("UP on " + PORT);
 });
